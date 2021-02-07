8aa40bd07527fd56ec1cc8dc99f86eab6a294af6
EnzoGhisoni <enzo.ghisoni@hotmail.fr>
2021-02-07 10:50:25 +0100

    Add a script to control the robot with the trackpad
    The new script allows to control the robot with the track Pad of the HTC Vive controllers

---
+++ src/billy_ros_sharp/src/vr_trackpad_teleoperation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from nav_msgs.msg import OccupancyGrid, Odometry, MapMetaData
+from geometry_msgs.msg import Vector3, Twist
+from visualization_msgs.msg import Marker
+from cv_bridge import CvBridge, CvBridgeError
+import sys
+bridge = CvBridge()
+"""
+roslaunch billy_ros_sharp billy_rossharp_vr_control.launch
+"""
+class VrTeloperation():
+	def __init__(self):
+		rospy.init_node("dpad_teloperation", anonymous=True)
+		self.bridge = CvBridge()
+		
+		self.vive_left_button_sub = rospy.Subscriber('/left_vive_trackpad', Vector3, self.vive_left_track_callback)
+		self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+		self.vive_left_dpad = Vector3()
+		
+		
+	
+	def vive_left_track_callback(self, ros_msg):
+		if(ros_msg.x !=0 and ros_msg.y !=0):
+			print('RobotMove')
+		self.vive_left_dpad = ros_msg
+		command = Twist()
+		command.linear.x = ros_msg.x
+		command.angular.z = ros_msg.y
+
+		self.cmd_vel.publish(command)
+
+
+def main(args):
+	VrTeloperation()
+	try:
+		rospy.spin()
+	except rospy.KeyboardInterrupt:
+		print("Shutting down")
+	
+
+if __name__ == '__main__':
+	main(sys.argv)
