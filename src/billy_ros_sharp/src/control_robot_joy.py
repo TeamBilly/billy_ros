e452970aa0af9233ebda697f590fb4adf49f7004
EnzoGhisoni <enzo.ghisoni@hotmail.fr>
2021-02-09 22:20:50 +0100

    Add script to control the robot with the joy

--- src/billy_ros_sharp/src/control_robot_joy.py
+++ src/billy_ros_sharp/src/control_robot_joy.py
@@ -3,22 +3,22 @@ import rospy
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Twist
+from sensor_msgs.msg import Joy
 
-
-class ControlRobotFromHeadsetSpeed:
+class ControlRobotJoy:
 	def __init__(self):
-		rospy.init_node('control_robot')
-		self.headsetSpeed_sub = rospy.Subscriber('/headsetSpeed', Twist, self.headsetSpeed)
+		rospy.init_node('control_robot_joy')
+		self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
 		self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 		self.robotSpeed = Twist()
 		
 
-	def headsetSpeed(self, msg):
-		self.robotSpeed.linear.x = msg.angular.x / 10 
-		self.robotSpeed.angular.z = msg.angular.y / 10
+	def joy_callback(self, msg):
+		self.robotSpeed.linear.x = msg.axes[1]
+		self.robotSpeed.angular.z = -msg.axes[0]
 		print('new Twist get !')
 		self.cmd_vel.publish(self.robotSpeed)
 
 if __name__ == '__main__':
-	ControlRobotFromHeadsetSpeed()
+	ControlRobotJoy()
 	rospy.spin()
